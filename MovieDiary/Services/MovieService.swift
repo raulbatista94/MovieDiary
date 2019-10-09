af45075afbd99f25b7a3689b194fcb1576dfc0f5
Raul Batista <raul@brightify.org>
2019-10-09 20:59:44 +0200

    Add initial implementation of movie service.

--- MovieDiary/Services/MovieService.swift
+++ MovieDiary/Services/MovieService.swift
@@ -6,6 +6,18 @@
 //  Copyright © 2019 Raul. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+import RxSwift
 
-
+final class MovieService {
+    // If possible get rid of this
+    private var moviePage: Int = 1
+    
+    private func getPopularMovies() -> Observable<[Movie]> {
+        return Observable.create { emitter in
+            //TODO
+//            request(<#T##url: URLConvertible##URLConvertible#>, method: <#T##HTTPMethod#>, parameters: <#T##Parameters?#>, encoding: <#T##ParameterEncoding#>, headers: <#T##HTTPHeaders?#>)
+            return Disposables.create()
+        }
+    }
+}
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '10.0'
+platform :ios, '9.0'
 
 target 'MovieDiary' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -9,7 +9,7 @@ target 'MovieDiary' do
   
   pod 'RxSwift', '~> 5'
   pod 'RxCocoa', '~> 5'
-  pod 'Alamofire', '~> 5.0.0-rc.2'
+  pod 'Alamofire', '~> 4.0'
 
   target 'MovieDiaryTests' do
     inherit! :search_paths
