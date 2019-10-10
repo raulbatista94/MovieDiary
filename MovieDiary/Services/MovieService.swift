7e9f70cd0bb8129496f3f61044e515cf39147712
Raul Batista <raul@brightify.org>
2019-10-10 13:55:24 +0200

    Add movie get function.

--- MovieDiary/Services/MovieService.swift
+++ MovieDiary/Services/MovieService.swift
@@ -8,16 +8,51 @@
 
 import Alamofire
 import RxSwift
+import Foundation
+
+enum MovieServiceError: LocalizedError {
+    case noDataReceived
+
+    var errorDescription: String? {
+        switch self {
+        case .noDataReceived:
+            return "No data received. Please check your internet connection."
+        }
+    }
+}
 
 final class MovieService {
     // If possible get rid of this
     private var moviePage: Int = 1
-    
-    private func getPopularMovies() -> Observable<[Movie]> {
+    private let constants = Constants()
+    private func getPopularMovies() -> Observable<MovieList> {
         return Observable.create { emitter in
-            //TODO
-//            request(<#T##url: URLConvertible##URLConvertible#>, method: <#T##HTTPMethod#>, parameters: <#T##Parameters?#>, encoding: <#T##ParameterEncoding#>, headers: <#T##HTTPHeaders?#>)
+            request(self.constants.baseApiUrlString,
+                    method: .get,
+                    parameters: ["api_key": self.constants.apiKey])
+                .responseJSON(completionHandler: { response in
+                    if let error = response.error {
+                        emitter.onError(error)
+                    } else if let data = response.data,
+                        let movieListDTO = try? JSONDecoder().decode(MovieListDTO.self, from: data) {
+                        return emitter.onNext(self.movieList(from: movieListDTO))
+                    } else {
+                    }
+                })
             return Disposables.create()
         }
+    }
+
+    private func movieList(from movieListDTO: MovieListDTO) -> MovieList {
+        return MovieList(page: movieListDTO.page ?? 0,
+                         movieResults: movieListDTO.results.map { self.movie(from: $0) },
+                         totalPages: movieListDTO.totalPages ?? 0)
+    }
+
+    private func movie(from movieDTO: MovieDTO) -> Movie {
+        return Movie(title: movieDTO.title ?? "",
+                     poster: movieDTO.posterPath ?? "",
+                     averageScore: movieDTO.averageScore ?? 0,
+                     overview: movieDTO.overview ?? "")
     }
 }
