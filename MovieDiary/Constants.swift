f5ee1b90be47128b0914ccaa9f36e6ee38886fb8
Raul Batista <raul@brightify.org>
2019-10-10 13:50:34 +0200

    Add constants to project.

---
+++ MovieDiary/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MovieDiary
+//
+//  Created by Raul Batista on 10/10/2019.
+//  Copyright © 2019 Raul. All rights reserved.
+//
+
+struct Constants {
+    let baseApiUrlString = "https://api.themoviedb.org/3/movie/popular"
+    let apiKey = "7052bb50967ad5557b8a76ea42e7234b"
+}
